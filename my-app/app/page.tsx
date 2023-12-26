ba7d67e4a6cb19c842caf2063ba396c00895fa0c
Doragon-RH <r216af@icloud.com>
2023-12-26 14:20:08 +0900

    JSX不足によるエラー出力

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -115,6 +115,18 @@ export default function Home() {
       <h1>Amazing scientists</h1>
       <Profile />
     </div>
+    // This doesn't quite work!
+    <h1>Hedy Lamarr's Todos</h1>
+    <img
+      src="https://i.imgur.com/yXOvdOSs.jpg"
+      alt="Hedy Lamarr"
+      class="photo"
+    >
+    <ul>
+      <li>Invent new traffic lights
+      <li>Rehearse a movie scene
+      <li>Improve spectrum technology
+    </ul>
     </main>
   )
 }
